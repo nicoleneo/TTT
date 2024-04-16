8300ce9e0df8fe2aaceb6fb3113ef535dd750856
Nicole Neo <nicolen@gmail.com>
2024-04-16 10:00:43 +0100

    serve frontend from backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,26 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import gameImpl from "./gameImpl";
+import path from 'path';
 
+// TODO: game session and store session for multiplayer
 
 const app: Express = express();
-const port = 3000;
+const port = 8080;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
-
 app.get("/start-game", (req: Request, res: Response)  => {
 	res.send(gameImpl.startGame());
 });
+/* serve built frontend (symlink ../../frontend/dist) */
+app.use("/frontend", express.static("frontend"));
+
+app.get('/frontend/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
